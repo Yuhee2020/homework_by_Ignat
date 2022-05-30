9740055fde345388631c605219dbb12f141adb25
Yukhimuk Dzmitry <yuhimuk.d@gmail.com>
2022-05-30 21:57:38 +0300

    homework9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,31 +1,30 @@
 import React, {useState} from 'react'
-import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {MyButton} from "../../p1-main/MyButton/MyButton";
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
-
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date.toLocaleTimeString()// fix with date
+    const stringDate = date.toLocaleDateString() // fix with date
 
     return (
         <div>
@@ -36,14 +35,15 @@ function Clock() {
                 {stringTime}
             </div>
 
-            {show && (
+
+            {show ? (
                 <div>
                     {stringDate}
                 </div>
-            )}
+            ) : <div>-</div>}
+            <MyButton name={"start"} callback={start}/>
+            <MyButton name={"stop"} callback={stop}/>
 
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
 
         </div>
     )
